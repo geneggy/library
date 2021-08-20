eff315322a4ad13eff63953865b844b1021af4a5
Eugene Gian <eugene.gian@outlook.com>
2021-08-19 22:43:24 -0700

    need to fix remove from array

--- script.js
+++ script.js
@@ -1,31 +1,6 @@
-let myLibrary = [
-  // { title: "Harry Potter", author: "JK Rowling", pages: "1983423", read: true },
-  // {
-  //   title: "Harry Potter2",
-  //   author: "JK Rowling",
-  //   pages: "1983423",
-  //   read: false,
-  // },
-  // {
-  //   title: "Harry Potter3",
-  //   author: "JK Rowling",
-  //   pages: "1983423",
-  //   read: true,
-  // },
-  // {
-  //   title: "Harry Potter3",
-  //   author: "JK Rowling",
-  //   pages: "1983423",
-  //   read: true,
-  // },
-  // {
-  //   title: "Harry Potter3",
-  //   author: "JK Rowling",
-  //   pages: "1983423",
-  //   read: true,
-  // },
-];
 const booksContainer = document.querySelector(".books-grid");
+const bookForm = document.querySelector(".modal-form");
+const myLibrary = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -57,71 +32,17 @@ function Book(title, author, pages, read) {
   this.bookCardEl.appendChild(this.pagesEl);
   this.bookCardEl.appendChild(this.readEl);
   this.bookCardEl.appendChild(this.deleteBtnEl);
-  // this.deleteBtnEl.addEventListener("click", this.delete);
+
+  this.deleteBtnEl.addEventListener("click", this.delete);
 }
 
-// Book.prototype.delete = () => {
-//   this.bookCardEl.remove();
-//   const index = myLibrary.indexOf(this);
-//   myLibrary.splice(index, 1);
-// };
-/*function addBook(book) {
-  const bookCard = document.createElement("div");
-  const title = document.createElement("h2");
-  const author = document.createElement("h3");
-  const pages = document.createElement("h3");
-  const read = document.createElement("p");
-  const deleteBtn = document.createElement("button");
-
-  bookCard.classList.add("book-card");
-  title.classList.add("book-title");
-  author.classList.add("book-author");
-  pages.classList.add("book-pages");
-  read.classList.add("book-read");
-  deleteBtn.classList.add("book-delete");
-  bookCard.setAttribute("data-index", myLibrary.indexOf(book));
-
-  title.textContent = `${book.title}`;
-  author.textContent = `${book.author}`;
-  pages.textContent = `${book.pages} pages`;
-  read.textContent = book.read ? "read" : "unread";
-  deleteBtn.textContent = "Remove Book";
-
-  bookCard.appendChild(title);
-  bookCard.appendChild(author);
-  bookCard.appendChild(pages);
-  bookCard.appendChild(read);
-  bookCard.appendChild(deleteBtn);
-
-  booksContainer.appendChild(bookCard);
-} */
-
-// window.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   if (e.target.classList.contains("book-delete")) {
-//     console.log(e.target.parentNode);
-//     console.log(myLibrary);
-//     //remove book from library - this doesnt work becuase after you remove one element the entire array shifts which throws off data-index connection
-//     removeBookFromLibrary(e.target.parentNode.dataset.index);
-//     //remove entire book from dom
-//     e.target.parentNode.remove();
-//   }
-// });
-
-// function renderBooks(books) {
-//   books.forEach((book) => {
-//     new Book(book);
-//     // addBook(book);
-//   });
-// }
-
-// function addBookToLibrary(book) {
-//   myLibrary.push(book);
-// }
-
-// function removeBookFromLibrary(book) {
-//   myLibrary.splice(index, 1);
-// }
+Book.prototype.delete = (e) => {
+  e.target.parentNode.remove();
+  const index = myLibrary.indexOf(this);
+  console.log(this);
+  myLibrary.splice(index, 1);
+  console.log(myLibrary);
+};
 
 //modal
 const modalContainer = document.querySelector(".modal-container");
@@ -141,15 +62,17 @@ window.addEventListener("keydown", (event) => {
 });
 
 //modal submit form
-const bookForm = document.querySelector(".modal-form");
 
 bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
   const title = document.querySelector("#title-input").value;
   const author = document.querySelector("#author-input").value;
   const pages = document.querySelector("#pages-input").value;
   const read = document.querySelector("#read-input").checked;
   const newBook = new Book(title, author, pages, read);
+  console.log(newBook);
   myLibrary.push(newBook);
+
   booksContainer.appendChild(newBook.bookCardEl);
   // addBookToLibrary(newBook);
   // addBook(newBook);
